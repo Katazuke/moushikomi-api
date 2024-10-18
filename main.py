449e530a9600247514863a35ec51e3ee75e3c86d
Katazuke <79079741+Katazuke@users.noreply.github.com>
2024-10-18 13:49:40 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import requests
 
 # 送信先のURL
-url = 'https://moushikomi-uketsukekun.com/maintenance_company/api/v2/entry_heads/3637058'
+url = 'https://moushikomi-uketsukekun.com/maintenance_company/api/content_images/19086285'
 
 # ヘッダ情報を定義（Authorizationヘッダを含む）
 headers = {
-    'Authorization': 'Token 5a5030e472a8f92a87e4e093f4161944',
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
+    'Authorization': 'Bearer 5a5030e472a8f92a87e4e093f4161944'  # 'YOUR_ACCESS_TOKEN'を実際のトークンに置き換えてください
 }
 
 # GETリクエストを送信（ヘッダを含む）
@@ -17,4 +15,4 @@ response = requests.get(url, headers=headers)
 print('ステータスコード:', response.status_code)
 
 # レスポンス内容の表示（テキスト形式）
-print('レスポンス本文:', response.text)文:', response.text)
+print('レスポンス本文:', response.text)
