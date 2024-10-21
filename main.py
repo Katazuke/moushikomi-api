045b5149d13c4236aa091adcd6ef7023884dc625
Katazuke <79079741+Katazuke@users.noreply.github.com>
2024-10-22 02:59:04 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
 	# レスポンス内容の表示（テキスト形式）
 	print('レスポンス本文:', res.text)
 
-	return
+	return res.text  #レスポンスを返す
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
