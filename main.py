fcb964df517397c1001c348d31caca05358394fe
Katazuke <79079741+Katazuke@users.noreply.github.com>
2024-10-22 02:29:04 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ def main():
 	#ヘッダ情報を定義（Authorizationヘッダを含む）
 	headers = {
     	'Authorization': 'Token 5a5030e472a8f92a87e4e093f4161944',
-	
 	}
-	
+
 	# GETリクエストを送信（ヘッダを含む）
 	res = requests.get(url, headers=headers)
 
@@ -22,6 +21,8 @@ def main():
 
 	# レスポンス内容の表示（テキスト形式）
 	print('レスポンス本文:', response.text)
+
+	return
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
