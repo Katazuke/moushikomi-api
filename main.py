6693ef216f3d7fb292b5c670a8417a6adb89af0c
Katazuke <79079741+Katazuke@users.noreply.github.com>
2024-10-22 03:15:39 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def main():
 	# 送信先のURL
 	url = 'https://moushikomi-uketsukekun.com/maintenance_company/api/content_images/19086285'
-        iapikey = 'Bearer 5a5030e472a8f92a87e4e093f4161944'
-        
+	iapikey = 'Bearer 5a5030e472a8f92a87e4e093f4161944'
+	
 	#ヘッダ情報を定義（Authorizationヘッダを含む）
 	headers = {'Authorization': iapikey}
 
