7ce3a132a140916c245303c1124c346812084b20
Katazuke <79079741+Katazuke@users.noreply.github.com>
2024-10-22 12:21:50 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def main():
 	# 送信先のURL
-	url = 'https://moushikomi-uketsukekun.com/maintenance_company/api/v2/entry_heads/3637058'
-	iapikey = 'Token 5a5030e472a8f92a87e4e093f4161944'
+	url = 'http://checkip.dyndns.com/'
+	#iapikey = 'Token 5a5030e472a8f92a87e4e093f4161944'
 	
 	#ヘッダ情報を定義（Authorizationヘッダを含む）
 	headers = {'Authorization': iapikey}
 
 	# GETリクエストを送信（ヘッダを含む）
-	res = requests.get(url, headers=headers)
+	#res = requests.get(url, headers=headers)
+	res = requests.get(url)
 
 	# ステータスコードの表示
 	print('ステータスコード:', res.status_code)
