d6d9b858253f14f96c6b79bc8ac62b09b7a10c2e
Katazuke <79079741+Katazuke@users.noreply.github.com>
2024-10-22 16:56:40 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def main():
 
 	# application_idが指定されていない場合はエラーを返す
 	if not application_id:
-		return "Error: 'application_id' parameter is required.", 400
+		return f"Error: 'application_id' parameter is required.", 400
 
 	# 送信先のURLを構築
-	url = 'https://moushikomi-uketsukekun.com/maintenance_company/api/v2/entry_heads/{application_id}'
+	url = f'https://moushikomi-uketsukekun.com/maintenance_company/api/v2/entry_heads/{application_id}'
 
 
 	# 送信先のURL
@@ -33,9 +33,7 @@ def main():
 	
 	# エラーハンドリング
 	if res.status_code != 200:
-		return f"Error: {res.status_code} - {res.text}",
-		res.status_code
-
+		return f"Error: {res.status_code} - {res.text}", res.status_code
 	# レスポンス内容の表示（JSON形式）
 	print('レスポ本文:', res.json())  # JSON形式での表示
 
