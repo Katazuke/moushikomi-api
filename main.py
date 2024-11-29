4142b9ce1cd201c586bc559c869b444f11e17b4d
Katazuke <79079741+Katazuke@users.noreply.github.com>
2024-11-29 16:55:47 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -650,7 +650,7 @@ def main():
 	app_data = map_variables(appjson, APPLICATION_COLUMNS_MAPPING)
 	app_data["Contractor__c"]=contractor_id
 	app_data["IndividualCorporation__c"]=renter_type
-	app_data["GuaranteePlan__c"]=plan_record
+	app_data["GuaranteePlan__c"]=plan_record_id
 	logging.info(f"app_data={app_data}")
 
 	for i in range(1, 6):  # 入居者 1〜5 をループ処理
