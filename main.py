98099af87325c49459281c755aa6a81a2bbd1e28
Katazuke <79079741+Katazuke@users.noreply.github.com>
2024-12-04 15:25:07 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -641,6 +641,7 @@ def split_company_and_branch(lines):
 	pattern = r"^(.*?株式会社(?: [^本店]*)?)\s*(本店|.*店)?$"
 	
 	for line in lines:
+		logging.info(f"line: {line}")
 		if not isinstance(line, str):  # 入力が文字列でない場合の処理
 			logging.warning(f"Invalid line input for splitting: {line}")
 			results.append((line, ""))  # デフォルト値で追加
